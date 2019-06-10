3b8015f8cd95a9987222397ce038978e36583905
civilizator <stan@argaatas.com>
2019-06-10 14:02:00 +0800

    Create

--- server.js
+++ server.js
@@ -3,7 +3,18 @@ const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
 const app = express();
 
+
 const port = 8000;
+
+require('./app/routes')(app, {});
+
+module.exports = function(app, db) {
+  app.post('/notes', (req, res) => {
+
+    res.send('Hello')
+  });
+};
+
 app.listen(port, () => {
   console.log('We are live on ' + port);
 });
