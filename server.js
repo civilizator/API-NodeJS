1d2c17bd4878eab1cf1a9f8856fa918b40a22001
civilizator <stan@argaatas.com>
2019-06-13 02:37:28 +0800

    Update

--- server.js
+++ server.js
@@ -11,9 +11,9 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 MongoClient.connect(db.url, (err, database) => {
     if (err) {return console.log(err)}
-    const db = database.db("note-api");
+    const db = database.db("hey");
     require('./app/routes')(app, db);
     app.listen(port, () => {
-        console.log('We are live on ' + port);
+        console.log(`We are live on: ${port}`);
     });
 });
