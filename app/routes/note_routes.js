577034db68b40006cfa8a78eab596a2535a20d1d
civilizator <stan@argaatas.com>
2019-06-13 03:33:13 +0800

    Update

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,19 +1,22 @@
-module.exports = function(app, db) {
-  app.post('/notes', (req, res) => {
-    // console.log(req.body);
-    // res.send('Hello');
-    const note = {
-      text: req.body.body,
-      title: req.body.title
-    };
-    db.collection('notes').insert(note, (err, result)=> {
-        if(err) {
-          res.send({'error': 'An error has occurred'})
-        }else {
-          res.send(result.ops[0])
-        }
+module.exports = function (app, db) {
+    app.post('/notes', (req, res) => {
+        // console.log(req.body);
+        // res.send('Hello');
+
+        const note = {
+            text: req.body.body,
+            title: req.body.title,
+            names: req.body.names
+        };
+
+        db.collection('notes').insertOne(note, (err, result) => {
+            if (err) {
+                res.send({'error': 'An error has occurred'})
+            } else {
+                res.send(result.ops[0])
+            }
+        });
     });
-  });
 };
 
 
--- server.js
+++ server.js
@@ -2,17 +2,22 @@ const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
 const app = express();
-const db = require('./config/db');
+const dbUrl = require('./config/db');
 
 const port = 8000;
 
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-MongoClient.connect(db.url, (err, database) => {
-    if (err) {return console.log(err)}
-    const db = database.db("hey");
+MongoClient.connect(dbUrl.url, {useNewUrlParser: true } , (err, database) => {
+    if (err) {
+        return console.log(err)
+    }
+
+    const db = database.db("parent");
+
     require('./app/routes')(app, db);
+
     app.listen(port, () => {
         console.log(`We are live on: ${port}`);
     });
