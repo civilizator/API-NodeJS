d6d0f23844cbebf5f68da47ef9a6757f4a466d86
civilizator <stan@argaatas.com>
2019-06-13 02:18:42 +0800

    Update

--- config/db.js
+++ config/db.js
@@ -1,3 +1,6 @@
+
+const data = require('./../password');
+
 module.exports = {
-  url: `mongodb+srv://stan:<password>@cluster0-nn4i1.mongodb.net/test?retryWrites=true&w=majority`
+  url: `mongodb+srv://stan:${data.password}@cluster0-nn4i1.mongodb.net/test?retryWrites=true&w=majority`
 };
